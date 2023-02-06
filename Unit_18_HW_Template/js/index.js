fa717fa5c2c770b69f2fcf430b79c0470cf1cd48
deathrae <122334309+deathrae@users.noreply.github.com>
2023-02-06 15:52:13 -0500

    Update index.js

--- Unit_18_HW_Template/js/index.js
+++ Unit_18_HW_Template/js/index.js
@@ -1,8 +1,6 @@
 console.log("Your JS file is loaded correctly!");
 
-content.addEventListener("mouseenter", function() {
-    content.style.backgroundColor = "lightgray";
-});
-content.addEventListener("mouseleave", function(){
-    content.style.backgroundColor = "white";
-});
+$("#toggle").on("click", function(){
+    $("#dropDown").slideToggle();
+  });
+  
