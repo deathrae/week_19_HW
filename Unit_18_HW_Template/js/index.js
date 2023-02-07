535dfff19686cbc4307d98393727bca619b09564
deathrae <122334309+deathrae@users.noreply.github.com>
2023-02-07 12:01:31 -0500

    Updating the script in HTML

--- Unit_18_HW_Template/js/index.js
+++ Unit_18_HW_Template/js/index.js
@@ -1,13 +1,11 @@
 console.log("Your JS file is loaded correctly!");
 
-hoverMe.addEventListener("mouseenter", myFunction)
-
 $(".hoverMe").mouseenter(function myFunction() {
   $(this).css("background-color", "pink");
 });
 
 $(".hoverMe").mouseleave(function myFunction() {
-  $(this).css("background-color", "dark-gray");
+  $(this).css("background-color", "black");
 });
 $(".hoverMe").on("click", function myFunction(){
   $(this).css("background-color", "gray");
