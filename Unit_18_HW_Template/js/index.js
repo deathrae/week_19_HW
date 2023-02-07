7727f2006f81d9de583d1a1f1e59d7d7b7ab46c0
deathrae <122334309+deathrae@users.noreply.github.com>
2023-02-07 11:52:19 -0500

    Updating the js to hover states

--- Unit_18_HW_Template/js/index.js
+++ Unit_18_HW_Template/js/index.js
@@ -1,12 +1,14 @@
 console.log("Your JS file is loaded correctly!");
 
-$(".hoverMe").mouseenter(function() {
-  alert("Hello World");
+hoverMe.addEventListener("mouseenter", myFunction)
+
+$(".hoverMe").mouseenter(function myFunction() {
+  $(this).css("background-color", "pink");
 });
 
-$(".hoverMe").mouseleave(function() {
-  alert("Hello World");
+$(".hoverMe").mouseleave(function myFunction() {
+  $(this).css("background-color", "dark-gray");
 });
-$(".hoverMe").on("click", function(){
-  $(this).hide();
+$(".hoverMe").on("click", function myFunction(){
+  $(this).css("background-color", "gray");
 })
